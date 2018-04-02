8abc1d39d9734d92003fd4cc411e7cb5ceaf7d9f
sanfly <sanfly@gmail.com>
2018-04-02 19:06:59 -0400

    Promopods export fix

--- Generator/promopods_au_v.php
+++ Generator/promopods_au_v.php
@@ -6,9 +6,11 @@ include("includes/header_web_au.php");
 if(isset($_GET['site'])){
     if(strtolower($_GET['site']) == "nz"){
         $site = "http://www.thermofisher.co.nz";
+        $country = "New Zealand";
     }
     elseif(strtolower($_GET['site']) == "au"){
         $site = "http://www.thermofisher.com.au";
+        $country = "Australia";
     }
 }
 
@@ -23,9 +25,9 @@ if(isset($_GET['site'])){
 
 include ("promopods_au_bare.php");
 
-
-
 ?>
+
+<input type="hidden" id="country" value="<?=$country;?>">
 
 
 <table width="700" border="0" cellspacing="0" cellpadding="0">
@@ -33,8 +35,8 @@ include ("promopods_au_bare.php");
         <tr valign="bottom">
             <td height="20">
             	<br><br><br><br>
-            	<a href="promopods_e.php?title=<?php echo $_GET['title']; ?>&site=<?php echo $_GET['site']; ?>" class="btn btn-commerce">Edit</a>
-            	<a href="promopods_export.php?title=<?=$_GET['title']; ?>&site=<?php echo $_GET['site']; ?>" class="btn btn-commerce">Export</a>
+            	<a href="promopods_e.php?title=<?php echo $_GET['title']; ?>&site=<?php echo $_GET['site']; ?>" class="btn btn-commerce local">Edit</a>
+            	<a href="promopods_export.php?title=<?=$_GET['title']; ?>&site=<?php echo $_GET['site']; ?>" class="btn btn-commerce local">Export</a>
             </td>
         </tr>
   </tbody>
